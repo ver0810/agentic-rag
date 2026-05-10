118dcf0524768f10cf4f84bf9aa8ff5ae2c893e2
ver0810 <15726533+ver0810@user.noreply.gitee.com>
2026-05-10 16:52:31 +0800

    fix: add knowledge mapper scan to MyBatisPlusConfig

--- bootstrap/src/main/java/com/agenticrag/user/config/MyBatisPlusConfig.java
+++ bootstrap/src/main/java/com/agenticrag/user/config/MyBatisPlusConfig.java
@@ -4,6 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.agenticrag.user.dao.mapper")
+@MapperScan({"com.agenticrag.user.dao.mapper", "com.agenticrag.knowledge.dao.mapper"})
 public class MyBatisPlusConfig {
 }
